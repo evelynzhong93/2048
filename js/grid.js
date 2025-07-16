40c20b9270f44f7fffcc66d0ece87fbe021bdd33
Yiying <142329093+yiying909@users.noreply.github.com>
2025-07-16 12:02:33 -0400

    finishing grid

--- js/grid.js
+++ js/grid.js
@@ -1,7 +1,7 @@
 class Grid {
     constructor(size, previousState) {
-        this.size = size
-        this.cells = previousState ? this.fromState(previousState) : this.empty
+        this.size = size;
+        this.cells = previousState ? this.fromState(previousState) : this.empty;
     }
 
     fromState(state) {
@@ -10,7 +10,7 @@ class Grid {
         for (let x = 0; x < this.size; x++) {
             const row = cells[x] = [];
             for (let y = 0; y < this.size; y++) {
-                tile = state[x][y]
+                tile = state[x][y];
                 row.push(tile ? new Tile(tile.value, tile.position) : null);
             }
         }
@@ -32,7 +32,7 @@ class Grid {
         return cells;
     }
 
-    availableCells() {
+    availableCellsLst() {
         let avCells = [];
 
         for (let x = 0; x < this.size; x++) {
@@ -46,7 +46,46 @@ class Grid {
         return avCells;
     }
 
-    randomAvailableCell() {
+    availableCellsBool() {
+        return this.availableCellsLst().length > 0;
+    }
 
+    randomAvailableCell() {
+        const avCells = this.availableCellsLst();
+        if (!avCells.length) { return null };
+        return avCells[Math.floor(Math.random() * avCells.length)];
+    }
+
+    //cell avaiable, occupied, content not incluced here, check original code
+
+    insertTile(tile) {
+        this.cells[tile.x][tile.y] = tile;
+    }
+
+    removeTile(tile) {
+        this.cells[tile.x][tile.y] = null;
+    }
+
+    withinBounds(position) {
+        return position.x >= 0 && position.x < this.size &&
+            position.y >= 0 && position.y < this.size;
+    }
+
+    serialize() {
+        var cellState = [];
+
+        for (let x = 0; x < this.size; x++) {
+            let row = cellState[x] = [];
+
+            for (let y = 0; y < this.size; y++) {
+                row.push(this.cells[x][y] ? this.cells[x][y].serialize() : null);
+            }
+        }
+
+        return {
+            size: this.size,
+            cells: cellState
+        }
     }
 }
+
