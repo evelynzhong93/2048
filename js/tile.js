a501d496888178f1692f11de917c3cb44125b070
Yiying <142329093+yiying909@users.noreply.github.com>
2025-07-16 21:14:45 -0400

    update tile

--- js/tile.js
+++ js/tile.js
@@ -1,8 +1,30 @@
 class Tile {
-    constructor(value, position) {
-        this.value = value
-        this.position = position
+    constructor(position, value) {
+        this.x = position.x;
+        this.y = position.y;
+        this.value = value || 2;
+
+        this.previousPosition = null;
+        this.mergedFrom = null;
     }
 
+    savePosition() {
+        this.previousPosition = { x: this.x, y: this.y };
+    }
+
+    updatePosition(position) {
+        this.x = position.x;
+        this.y = position.y;
+    }
+
+    serialize() {
+        return {
+            position: {
+                x: this.x,
+                y: this.y
+            },
+            value: this.value
+        }
+    }
 
 }
